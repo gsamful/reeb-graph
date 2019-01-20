8115506b9e7f331e5ca7e94129a67027b987c2d4
Harvey,William <harveyw@oclc.org>
2019-01-20 13:31:32 -0500

    Initial work to load OFF file into data structures used by the algorithm.

--- src/Off.hs
+++ src/Off.hs
@@ -1,6 +1,7 @@
 module Off where
 
 import Text.Read
+import Simplex as S
 
 {-
 TODO:  This is an extremely rudimentary, and frankly extremely crappy, parser.
@@ -12,7 +13,20 @@ TODO:  This is an extremely rudimentary, and frankly extremely crappy, parser.
        And the return value should be a record rather than a tuple.
 -}
 
-parseOffFile :: [[Char]] -> Either String ([(Double, Double, Double)], [(Int, Int, Int)])
+data Point3D = Point3D
+  { x :: Double
+  , y :: Double
+  , z :: Double
+  } deriving (Show, Eq)
+
+data Face3 = Face3
+  { p :: Int
+  , q :: Int
+  , r :: Int
+  } deriving (Show, Eq)
+
+
+parseOffFile :: [[Char]] -> Either String ([Point3D], [Face3])
 parseOffFile lines = 
   do
     (line1, rest1)       <- nextLine lines
@@ -21,14 +35,14 @@ parseOffFile lines =
     (v, f, e)            <- parseVerticesFacesEdges line2
     (vertices, rest3)    <- case splitAt v rest2 of
                               (xs, ys) | length xs == v -> 
-                                fmap (\x -> (x, ys)) (sequence $ fmap parseVertex xs) :: Either String ([(Double, Double, Double)], [[Char]])
+                                fmap (\x -> (x, ys)) (sequence $ fmap parseVertex xs) :: Either String ([Point3D], [[Char]])
                               otherwise -> Left "Error: Not enough vertices."
     (triangles, rest4)   <- case splitAt f rest3 of
                               (xs, ys) | length xs == f -> 
-                                fmap (\x -> (x, ys)) (sequence $ fmap (\x -> parseTriangle x v) xs) :: Either String ([(Int, Int, Int)], [[Char]])
+                                fmap (\x -> (x, ys)) (sequence $ fmap (\x -> parseTriangle x v) xs) :: Either String ([Face3], [[Char]])
                               otherwise -> Left "Error: Not enough faces."
     result               <- case rest4 of
-                              [] ->        Right (vertices :: [(Double, Double, Double)], triangles :: [(Int, Int, Int)])
+                              [] ->        Right (vertices :: [Point3D], triangles :: [Face3])
                               otherwise -> Left "Error: Extra lines found at the end of the file."
 
     return result
@@ -51,16 +65,16 @@ parseVerticesFacesEdges line =
     [Just v, Just f, Just e] -> Right (v, f, e)
     otherwise                -> Left $ "Error: Expected '#vertices #faces #edges' but found '" ++ line ++ "'"
 
-parseVertex :: [Char] -> Either String (Double, Double, Double)
+parseVertex :: [Char] -> Either String Point3D
 parseVertex line = 
   case fmap (\word -> readMaybe word :: Maybe Double) $ words line of
-    [Just x, Just y, Just z] -> Right (x, y, z)
+    [Just x, Just y, Just z] -> Right $ Point3D x y z
     otherwise                -> Left $ "Error: Invalid vertex '" ++ line ++ "'"
 
-parseTriangle :: [Char] -> Int -> Either String (Int, Int, Int)
+parseTriangle :: [Char] -> Int -> Either String Face3
 parseTriangle line maxVertexId = 
   case fmap (\word -> readMaybe word :: Maybe Int) $ words line of
-    [Just 3, Just x, Just y, Just z] | vertexIndicesValid x y z -> Right (x, y, z)
+    [Just 3, Just p, Just q, Just r] | vertexIndicesValid p q r -> Right $ Face3 p q r
       where 
         vertexIndicesValid :: Int -> Int -> Int -> Bool
         vertexIndicesValid x y z = 
